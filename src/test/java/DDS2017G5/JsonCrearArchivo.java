a285c64f2219b1ed471b4ce53a9759c9ecc98eb7
PabloAntivero <rcentral101@hotmail.com>
2017-04-16 00:01:44 -0300

    Clase para generar el archivo JSON
    Subo clase para generar el Archivo JSON

---
+++ src/test/java/DDS2017G5/JsonCrearArchivo.java
@@ -0,0 +1,54 @@
+package DDS2017G5;
+
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonCrearArchivo {
+
+    public static void main(String[] args) {
+    	
+        /** Creo listas **/
+    	List<Empresa> listaEmpresas = new ArrayList<>();
+    	List<Cuenta> listaCuentas = new ArrayList<>();
+    	List<Indicador> listaIndicadores = new ArrayList<>();
+    	
+    	String nombre_cuenta1 = "EBDITA";
+    	String nombre_cuenta2 = "Free Cash Flow";
+    	/** creo Cuentas **/    	
+    	Cuenta cuenta1 = new Cuenta(nombre_cuenta1,200,LocalDate.now(),LocalDate.now());
+    	Cuenta cuenta2 = new Cuenta(nombre_cuenta2,100,LocalDate.parse("2017-01-01"),LocalDate.parse("2017-02-01"));
+    	listaCuentas.add(cuenta1);
+    	listaCuentas.add(cuenta2);
+    	/** creo empresas **/
+    	Empresa empresa1 = new Empresa("Facebook",listaCuentas,listaIndicadores);
+    	Empresa empresa2 = new Empresa("Google",listaCuentas,listaIndicadores);
+    	listaEmpresas.add(empresa1);
+    	listaEmpresas.add(empresa2);
+    	/** creo un repo aux **/
+    	
+    	RepoEmpresas repoEmpresasJson = new RepoEmpresas();
+    	repoEmpresasJson.setNombre("RepoEmpresasJson");
+    	repoEmpresasJson.agregarEmpresa(empresa1);
+    	repoEmpresasJson.agregarEmpresa(empresa2);
+    		  	
+    	    	
+        //1. Convert object to JSON string
+        Gson gson = new Gson();
+        String json = gson.toJson(repoEmpresasJson);
+        System.out.println(json);
+
+        //2. Convert object to JSON string and save into a file directly
+        try (FileWriter writer = new FileWriter("C:\\Users\\rcent_000\\Desktop\\RepoEmpresasJson.json")) {
+
+            gson.toJson(repoEmpresasJson, writer);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+

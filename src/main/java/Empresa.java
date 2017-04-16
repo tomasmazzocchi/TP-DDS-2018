38bae52597953ff78e68de0a98be5c829f5cfa42
PabloAntivero <rcentral101@hotmail.com>
2017-04-15 23:25:13 -0300

    Modifico Empresa
    Agrego Getter y Setter de la listas, y constructor

--- src/main/java/Empresa.java
+++ src/main/java/Empresa.java
@@ -1,16 +1,35 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import pruebadds2017.Cuenta;
+import pruebadds2017.Indicador;
+
 public class Empresa {
 	private String nombre;
 	private List<Cuenta> listaCuentas = new ArrayList<>();
 	private List<Indicador> listaIndicadores = new ArrayList<>();
 	
+	// Getters and Setters
 	public String getNombre() {
 		return nombre;
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+	public List<Cuenta> getListaCuentas(){
+		return this.listaCuentas;
+	}
+	public List<Indicador> getListaIndicadores(){
+		return this.listaIndicadores;
+				;
+	}
+	// Constructors
+	
+	public Empresa(String nombre,List<Cuenta> listaCuentas,List<Indicador> listaIndicadores) 
+	{
+		this.nombre = nombre;
+		this.listaCuentas = listaCuentas;
+		this.listaIndicadores = listaIndicadores;
 	}
 
 	

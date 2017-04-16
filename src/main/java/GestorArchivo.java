c4f8a7a8f775e006e1866e3f4b17b32fbb1876cb
PabloAntivero <rcentral101@hotmail.com>
2017-04-15 23:48:37 -0300

    Agrego Cargar Archivo en el gestor Archivo
    Cargar Archivo

--- src/main/java/GestorArchivo.java
+++ src/main/java/GestorArchivo.java
@@ -1,4 +1,37 @@
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class GestorArchivo {
 
+	public void cargarArchivo(RepoEmpresas repoEmpresas, String rutaArchivo) {
+
+        Gson gson = new Gson();
+         
+        try (Reader reader = new FileReader(rutaArchivo)) 
+        {
+            // new list JSON
+            List<Empresa> listaEmpresasJson = new ArrayList<Empresa>();	
+            
+            // Convert JSON to Java Object
+            RepoEmpresas repoEmpresasJson = gson.fromJson(reader, RepoEmpresas.class);
+            listaEmpresasJson = repoEmpresasJson.getListaEmpresa();
+            
+
+            Iterator<Empresa> iterador = listaEmpresasJson.listIterator();
+            //While Iterator has a next element
+              while( iterador.hasNext() ) {
+                     Empresa empresaJson = (Empresa) iterador.next();
+                     repoEmpresas.agregarEmpresa(empresaJson);
+                     // Manejar EXCEPCION si la empresa existe
+              }
+                  
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

84c6382a90675170ebf83d98a536cb73d06f9700
tmazzocchi <tomasmazzocchi@hotmail.com>
2017-05-11 21:59:14 -0300

    investigando sobre getresourceasstream()

--- src/test/java/ar/edu/utn/dds/grupo5/GestorArchivoTest.java
+++ src/test/java/ar/edu/utn/dds/grupo5/GestorArchivoTest.java
@@ -2,6 +2,8 @@ package ar.edu.utn.dds.grupo5;
 
 import org.junit.*;
 import org.junit.rules.ExpectedException;
+import java.io.*;
+import org.apache.commons.io.IOUtils;
 
 import ar.edu.utn.dds.grupo5.GestorArchivo;
 import ar.edu.utn.dds.grupo5.RepoEmpresas;
@@ -13,11 +15,21 @@ public class GestorArchivoTest {
 	private String rutaValida;
 	private String rutaInvalida;
 	private String rutaConJsonInvalido;
+	private InputStream hola;
 	
 	@Before
-	public void init(){
+	public void init() throws IOException{
 		repositorio = new RepoEmpresas("Bancos");
 		gestor = GestorArchivo.getInstance();
+		/*hola = hola.getClass().getResourceAsStream("Archivo Ivalido.json");
+		//rutaConJsonInvalido.getClass().getResourceAsStream("\\Archivo Invalido.json"); /*"\\GitHub\\2017-mn-group-05\\src\\test\\resources\\Archivo Invalido.json";*/
+		//rutaConJsonInvalido = IOUtils.toString(hola, "UTF-8");// .toString(hola);
+		/*StringWriter writer = new StringWriter();
+		IOUtils.copy(hola, writer, "UTF-8");
+		rutaConJsonInvalido = writer.toString();*/
+		
+		
+		//rutaConJsonInvalido = IOUtils.toString(InputStream.class.getResourceAsStream("\\Archivo Ivalido.json"),"UTF-8"); 
 		rutaConJsonInvalido = "\\GitHub\\2017-mn-group-05\\src\\test\\resources\\Archivo Invalido.json";
 		rutaValida = "\\GitHub\\2017-mn-group-05\\src\\test\\resources\\Archivo Valido.json";
 	    rutaInvalida = "C:\\Users\\Tomás\\Desktop\\Hola.json";
@@ -40,14 +52,14 @@ public class GestorArchivoTest {
 	public void siColocoUnaRutaInvalidaEntoncesNoSeCarganEmpresasAlRepositorio(){
 		thrown.expect(RuntimeException.class);
 		thrown.expectMessage("Ruta inexistente");
-		gestor.cargarArchivo(repositorio, rutaInvalida);
-		Assert.assertTrue(repositorio.getListaEmpresa().size()==0);
+		gestor.cargarArchivo(repositorio,rutaInvalida);
+		//Assert.assertTrue(repositorio.getListaEmpresa().size()==0);
 	}
 	@Test
 	public void siColocoUnaRutaValidaConUnJsonInvalidoEntoncesNoSeCarganEmpresasAlRepositorio(){
 		thrown.expect(RuntimeException.class);
 		thrown.expectMessage("Archivo invalido");
 		gestor.cargarArchivo(repositorio, rutaConJsonInvalido);
-		Assert.assertTrue(repositorio.getListaEmpresa().size()==0);
+		//Assert.assertTrue(repositorio.getListaEmpresa().size()==0);
 	}
 }

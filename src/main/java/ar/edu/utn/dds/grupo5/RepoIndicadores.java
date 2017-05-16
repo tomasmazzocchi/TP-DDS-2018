817304ac65f48bc9d9c860e03299d2c05314af05
PabloAntivero <rcentral101@hotmail.com>
2017-05-16 16:32:43 -0300

    Genero RepoIndicadores

---
+++ src/main/java/ar/edu/utn/dds/grupo5/RepoIndicadores.java
@@ -0,0 +1,50 @@
+package ar.edu.utn.dds.grupo5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.utn.dds.ExceptionHandler.IndicadorExistenteException;
+
+public class RepoIndicadores {
+	private String nombre;
+	private List<Indicador> listaIndicadores = new ArrayList<>();
+	
+	// Getters and Setters
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public List<Indicador> getListaIndicador(){
+		return this.listaIndicadores;
+	}
+
+	// Functions and Procedures
+    
+	public void reset() {
+		listaIndicadores.clear();
+	}
+	
+	public void agregarIndicador(Indicador unIndicador){
+		if(!listaIndicadores.contains(unIndicador)){
+			listaIndicadores.add(unIndicador);
+		} 
+		else{
+			throw new IndicadorExistenteException ("Indicador ya existente");
+		}
+	}
+	public RepoIndicadores(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void quitarIndicador(Indicador unaIndicador){
+		if(listaIndicadores.contains(unaIndicador)){
+			listaIndicadores.remove(unaIndicador);
+		} else{
+			throw new IndicadorExistenteException ("No existe el Indicador");
+		}
+	}
+}

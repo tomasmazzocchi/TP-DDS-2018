859b6f434a5a0d66aad566f341e70dfb8c99fafa
PabloAntivero <rcentral101@hotmail.com>
2017-05-16 17:30:15 -0300

    Agrego Validador Expresion
    en el Parser

--- src/main/java/ar/edu/utn/dds/grupo5/ExpressionParser.java
+++ src/main/java/ar/edu/utn/dds/grupo5/ExpressionParser.java
@@ -124,6 +124,80 @@ public class ExpressionParser {
         	throw new IllegalArgumentException();
         }
     }
+    
+    //**validador
+    /**
+     * Parses an expression into an integer result.
+     * @param expression the expression to part
+     * @return and integer result
+     */
+    public boolean validarExpresion(final String expression) {
+        /*
+         * Create a lexer that reads from our expression string
+         */
+        final SimpleLexer lexer = new SimpleLexer(new ANTLRInputStream(expression));
+
+        /*
+         * By default Antlr4 lexers / parsers have an attached error listener
+         * that prints errors to stderr. I prefer them to throw an exception
+         * instead so I implemented my own error listener which is attached
+         * here. I also remove any existing error listeners.
+         */
+        lexer.removeErrorListeners();
+        lexer.addErrorListener(_listener);
+
+        /*
+         * The lexer reads characters and lexes them into token stream. The
+         * tokens are consumed by the parser that then builds an Abstract
+         * Syntax Tree.
+         */
+        final CommonTokenStream tokens = new CommonTokenStream(lexer);
+        final SimpleParser parser = new SimpleParser(tokens);
+        parser.removeErrorListeners();
+        parser.addErrorListener(_listener);
+
+        /*
+         * The ExprContext is the root of our Abstract Syntax Tree
+         */
+        final ExprContext context = parser.expr();
+
+        /*
+         * 'Visit' all the branches of the tree to get our expression result.
+         */
+        return visitValidador(context);
+    }
+
+    /*
+     * Visits the branches in the expression tree recursively until we hit a
+     * leaf.
+     */
+    private boolean visitValidador(final ExprContext context) {
+    
+        if (context.number() != null) { //Just a number
+            return true;
+        }
+        else if (context.string() != null){
+           	return true;
+		}
+        else if (context.BR_CLOSE() != null) { //Expression between brackets
+            return visitValidador(context.expr(0));
+        }
+        else if (context.MULTIPLICAR() != null) { //Expression * expression
+            return visitValidador(context.expr(1));
+        }
+        else if (context.DIVIDIR() != null) { //Expression / expression
+            return visitValidador(context.expr(1));
+        }
+        else if (context.SUMAR() != null) { //Expression + expression
+            return visitValidador(context.expr(1));
+        }
+        else if (context.RESTAR() != null) { //Expression - expression
+            return visitValidador(context.expr(1));
+        }
+        else {
+        	throw new IllegalArgumentException();
+        }
+    }
 
     /*
      * Helper method to create an ANTLRErrorListener. We're only interested in
--- src/main/java/ar/edu/utn/dds/grupo5/ManejadorIndicadores.java
+++ src/main/java/ar/edu/utn/dds/grupo5/ManejadorIndicadores.java
@@ -2,11 +2,19 @@ package ar.edu.utn.dds.grupo5;
 
 public class ManejadorIndicadores {
 	ExpressionParser _parser;
+	Indicador indicador;
 	
 	public void guardarIndicadorEnEmpresa(Empresa empresa,String indicadorNombre,String indicadorFormula){
+		_parser.validarExpresion(indicadorFormula,empresa.getListaCuentas(),empresa.getListaIndicadores());
+		indicador = new Indicador(indicadorNombre,indicadorFormula);
+		empresa.getListaIndicadores().add(indicador);		
 		
-		try 
-		_parser.parse("cu.pepe/2",listaCuentas, listaIndicadores)
+	}
+	
+	public void guardarIndicadorEnRepo(RepoIndicadores repoIndicadores,String indicadorNombre,String indicadorFormula){
+		_parser.validarExpresion(indicadorFormula,empresa.getListaCuentas(),empresa.getListaIndicadores());
+		indicador = new Indicador(indicadorNombre,indicadorFormula);
+		empresa.getListaIndicadores().add(indicador);		
 		
 	}
 

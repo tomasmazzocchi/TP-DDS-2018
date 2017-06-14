6e97d30b1b596b84397928306e9286df95dd504a
tmazzocchi <tomasmazzocchi@hotmail.com>
2017-06-13 23:24:40 -0300

    Agrego algunos detalles a la pantalla MetodologiaExistente.fxml

--- src/main/java/ar/edu/utn/dds/GUI/MetodologiaExistenteController.java
+++ src/main/java/ar/edu/utn/dds/GUI/MetodologiaExistenteController.java
@@ -3,18 +3,38 @@ package ar.edu.utn.dds.GUI;
 
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 
-public class MetodologiaExistenteController {
+public class MetodologiaExistenteController implements Initializable {
 
 	@FXML private Button btnVolver;
+	@FXML private Button btnAplicar;
+	@FXML private ComboBox<String> comboBox;
+	@FXML private TableView<?> tblListado;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		comboBox.getItems().removeAll(comboBox.getItems());
+	    comboBox.getItems().addAll("Metodologia Buffet");
+	    comboBox.getSelectionModel().select("Metodologia Buffet");
+	}
+	
+	public void aplicarMetodologia(ActionEvent event) throws IOException{
+		if(event.getSource()==btnAplicar){
+			
+		}
+	}
 	
 	@FXML public void volver(ActionEvent event) throws IOException{
 		Parent home_page_parent = null;

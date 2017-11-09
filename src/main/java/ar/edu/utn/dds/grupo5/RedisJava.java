db663c9b173e59b6f571930ea5fda28a6b6ed13d
Tomas Mazzocchi <tomasmazzocchi@hotmail.com>
2017-11-09 18:39:13 -0300

    Redis probado y funcionando, falta integracion al tp. Subo .zip para instalar redis antes de correr la clase de redis

--- src/main/java/ar/edu/utn/dds/grupo5/RedisJava.java
+++ src/main/java/ar/edu/utn/dds/grupo5/RedisJava.java
@@ -3,6 +3,17 @@ package ar.edu.utn.dds.grupo5;
 import redis.clients.jedis.Jedis;
 
 public class RedisJava {
+	/*
+	public static void main(String[] args) {
+		  //Connecting to Redis on localhost
+		  Jedis jedis = new Jedis("localhost");
+		  //adding a new key
+		  jedis.set("key", "value");
+		  //getting the key value
+		  System.out.println(jedis.get("key"));
+	}
+		*/ 
+	
 
 	public static void main(String[] args) {
 		// Connecting to Redis server on localhost
@@ -12,6 +23,7 @@ public class RedisJava {
 		jedis.set("tutorial-name", "Redis tutorial");
 		// Get the stored data and print it
 		System.out.println("Stored string in redis:: " + jedis.get("tutorialname"));
-		jedis.close();
+		//jedis.close();
+		System.out.println(jedis.get("tutorial-name"));
 	}
 }
